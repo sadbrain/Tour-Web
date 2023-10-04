95593ba83dcde51f9fb3c7057130e9b7007b33f2
sadbrain <riotgheptrannhulol@gmail.com>
2023-10-03 22:07:09 -0700

    create file  repository.js

---
+++ DataAcess/repository.js
@@ -0,0 +1,62 @@
+
+// import { writeFileSync } from 'fs';
+// console.log(2);
+// // Tạo một mảng chứa thông tin giả cho 5 người dùng
+// const fakeLocations = [
+//   {
+//     id: 1,
+//     name: 'Paris',
+//     description: 'The romantic capital of the world known for its art, culture, and history.',
+//     priice: 'France',
+//     depature_day:
+//     durations:
+//     img: [
+
+//     ] ,
+//     category
+//   },
+//   {
+//     id: 2,
+//     name: 'Kyoto',
+//     country: 'Japan',
+//     description: 'A city of tradition and temples, famous for its cherry blossoms.',
+//     imgs: [
+      
+//     ]
+//   },
+//   {
+//     id: 3,
+//     name: 'New York City',
+//     country: 'United States',
+//     description: 'The city that never sleeps, a hub of finance, culture, and diversity.',
+//     imgs: [
+      
+//     ]
+//   },
+//   {
+//     id: 4,
+//     name: 'Sydney',
+//     country: 'Australia',
+//     description: 'A stunning harbor city with iconic landmarks like the Sydney Opera House.',
+//     imgs: [
+      
+//     ]
+//   },
+//   {
+//     id: 5,
+//     name: 'Rio de Janeiro',
+//     country: 'Brazil',
+//     description: 'A vibrant city known for its beaches, samba, and Christ the Redeemer statue.',
+//     imgs: [
+      
+//     ]
+//   },
+// ];
+
+
+// const jsonData = JSON.stringify(fakeUsers, null, 2);
+
+// // Ghi dữ liệu vào tệp JSON
+// writeFileSync('users.json', jsonData, 'utf-8');
+
+// console.log('Tạo tệp users.json thành công!');
