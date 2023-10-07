e9dee5c1b44d062f91e474e9e47f9cfb82617c8c
sadbrain <riotgheptrannhulol@gmail.com>
2023-10-07 05:40:04 -0700

    login with no hash

--- Lib/Page/Js/hashPassword.js
+++ Lib/Page/Js/hashPassword.js
@@ -1,11 +1,99 @@
-import { createHmac, randomBytes } from 'crypto';
+
+// import { createHmac } from "crypto"
+// export { hashPassword, salt };
+// function hashPassword(password, salt) {
+//   const hash = createHmac('sha256', salt).update(password).digest('hex');
+//   return hash;
+// }
+// const salt= "8b65b9403b9427421db5921a2b182eae";
+import { createHmac } from "crypto"
+
+const salt= "8b65b9403b9427421db5921a2b182eae";
+const usersAPi = "http://localhost:3000/users";
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const sumbit = document.querySelector("#sumbit");
+const encodedPassword = hashPassword(password.value, salt);
+console.log(encodedPassword); 
+console.log(hashPassword("asa"),salt);
 
 function hashPassword(password, salt) {
   const hash = createHmac('sha256', salt).update(password).digest('hex');
   return hash;
 }
+async function fetchDataFromAPI() {
+  try {
+    const response = await fetch(usersAPi, {
+      method: "GET", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-const salt = randomBytes(16).toString('hex'); // Tạo salt ngẫu nhiên
-const password = 'mySecurePassword'; // Mật khẩu của người dùng
-const hashedPassword = hashPassword(password, salt); // Mã hóa mật khẩu
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Failed to fetch data from the API");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+sumbit.onclick = async function login() {
+  try {
+    // Fetch data from the API
+    const data = await fetchDataFromAPI();
+
+    if (_checkUserValid(data)) {
+      alert("Đăng nhập thành công");
+      _setCurrentUser(data); // Define this function
+      _navigateToHome(); // Define this function for navigation
+    } else {
+      alert("Email hoặc mật khẩu của bạn sai, vui lòng nhập lại!");
+    }
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+
+function _checkUserValid(data) {
+
+  return data.some((element) => {
+    return element.email == email.value && element.password == password.value;
+  });
+}
+
+function _setCurrentUser(data) {
+  const element = data.find((element) => {
+    return element.email == email.value && element.password == password.value;
+  });
+  if (element != undefined) {
+    const obj = {
+      id: element.id,
+      role: element.id_role
+    }
+
+    localStorage.setItem("user_token", JSON.stringify(obj));
+    
+
+
+  }
+}
+
+function _navigateToHome() {
+  const user_token = JSON.parse(localStorage.getItem("user_token"));
+
+  if (user_token) {
+    if(user_token.role == 2) {
+      window.location.href = "./home.html";
+    }else{  
+
+      window.location.href = "../Page/Admin/home.html";
+     
+    }
+  }
+}
+
 
--- Lib/Page/Js/login.js
+++ Lib/Page/Js/login.js
@@ -1,34 +1,54 @@
-// import {hashPassword} from "./hashPassword.js";
+// import { createHmac } from "crypto"
+
+// const salt= "8b65b9403b9427421db5921a2b182eae";
 const usersAPi = "http://localhost:3000/users";
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-// const encodedPassword = hashPassword(password.value, process.env.PASSWORD_SALT);
-// console.log(encodedPassword);
-async function login() {
+const sumbit = document.querySelector("#sumbit");
+// const encodedPassword = hashPassword(password.value, salt);
+// console.log(encodedPassword); 
+// console.log(hashPassword("asa"),salt);
+
+function hashPassword(password, salt) {
+  const hash = createHmac('sha256', salt).update(password).digest('hex');
+  return hash;
+}
+async function fetchDataFromAPI() {
   try {
-    var response = await fetch(usersAPi);
+    const response = await fetch(usersAPi, {
+      method: "GET", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     if (response.ok) {
-      response.json().then((data) => {
-        if (_checkUserValid(data)) {
-          alert("đăng nhập thành công");
-
-          _setCurrentUser(data);
-          _navigateToHome();
-        } else {
-          alert("email hoặc password của bạn sai, vùi lòng nhập lại!");
-        }
-      });
-
-
-
-
+      const data = await response.json();
+      return data;
     } else {
+      throw new Error("Failed to fetch data from the API");
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+sumbit.onclick = async function login() {
+  try {
+    // Fetch data from the API
+    const data = await fetchDataFromAPI();
+
+    if (_checkUserValid(data)) {
+      alert("Đăng nhập thành công");
+      _setCurrentUser(data); // Define this function
+      _navigateToHome(); // Define this function for navigation
+    } else {
+      alert("Email hoặc mật khẩu của bạn sai, vui lòng nhập lại!");
     }
   } catch (err) {
     alert(err.message);
   }
 }
+
 
 function _checkUserValid(data) {
 
@@ -39,25 +59,30 @@ function _checkUserValid(data) {
 
 function _setCurrentUser(data) {
   const element = data.find((element) => {
-    console.log(email.value);
-    console.log(password.value);
     return element.email == email.value && element.password == password.value;
   });
   if (element != undefined) {
-    const arr = [{
+    const obj = {
       id: element.id,
-      role: element.role
-    }]
-    localStorage.setItem("user_token", arr);
+      role: element.id_role
+    }
+
+    localStorage.setItem("user_token", JSON.stringify(obj));
+    
+
+
   }
 }
 
 function _navigateToHome() {
-  if (localStorage.getItem("user_token")) {
-    if(localStorage.getItem("user_token").role == 2) {
+  const user_token = JSON.parse(localStorage.getItem("user_token"));
+
+  if (user_token) {
+    if(user_token.role == 2) {
       window.location.href = "./home.html";
-    }else{
-      window.location.href = "../Admin/home.html";
+    }else{  
+
+      window.location.href = "../Page/Admin/home.html";
      
     }
   }
