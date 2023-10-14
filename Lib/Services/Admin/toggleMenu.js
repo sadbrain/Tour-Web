9b694b9ab1c042eda75bc4c0346107c99fa9611b
sadbrain <riotgheptrannhulol@gmail.com>
2023-10-14 12:10:02 -0700

    finish userManagmeent

---
+++ Lib/Services/Admin/toggleMenu.js
@@ -0,0 +1,11 @@
+// toggle menu
+document.querySelector(".toggle-menu").onclick = () => {
+    document.querySelector('#wrapper').classList.toggle('toggled');
+    document.querySelector('.toggle-menu i').classList.toggle('content-wrapper');
+}
+
+document.querySelector(".dropdown-toggle").onclick = () => {
+    document.querySelector('.dropdown-menu-right').classList.toggle('show');
+}
+
+//back to top
